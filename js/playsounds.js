aa10e558add2b66178933b161827b984925ade0d
Martin Thoma <info@martin-thoma.de>
2014-12-08 11:16:19 +0100

    js/playsounds.js: Use timeout instead of buisy waiting

--- js/playsounds.js
+++ js/playsounds.js
@@ -2,24 +2,12 @@ function play_sound() {
     if (Math.random() < 0.5) {
         document.getElementById('results').innerHTML += 'left<br/>';
         document.getElementById('left_channel').play();
-        var start = new Date().getTime();
-        var milliseconds = document.getElementById('seconds').value;
-        for (var i = 0; i < 1e7; i++) {
-            if ((new Date().getTime() - start) > milliseconds){
-                break;
-            }
-        }
-        document.getElementById('right_channel').play();
+        setTimeout(function(){ document.getElementById('right_channel').play(); },
+                   document.getElementById('seconds').value);
     } else {
         document.getElementById('results').innerHTML += 'right<br/>';
         document.getElementById('right_channel').play();
-        var start = new Date().getTime();
-        var milliseconds = document.getElementById('seconds').value;
-        for (var i = 0; i < 1e7; i++) {
-            if ((new Date().getTime() - start) > milliseconds){
-                break;
-            }
-        }
-        document.getElementById('left_channel').play();
+        setTimeout(function(){ document.getElementById('left_channel').play(); },
+                   document.getElementById('seconds').value);
     }
 }
